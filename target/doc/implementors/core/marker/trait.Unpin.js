068cbd544c5c33adbeda6a69c5250566676ccbd0
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-11-09 10:55:53 +0700

    add documentation

---
+++ target/doc/implementors/core/marker/trait.Unpin.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["ringbuf"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"ringbuf/struct.Consumer.html\" title=\"struct ringbuf::Consumer\">Consumer</a>&lt;T&gt;",synthetic:true,types:["ringbuf::consumer::Consumer"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"ringbuf/struct.Producer.html\" title=\"struct ringbuf::Producer\">Producer</a>&lt;T&gt;",synthetic:true,types:["ringbuf::producer::Producer"]},{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"ringbuf/struct.RingBuffer.html\" title=\"struct ringbuf::RingBuffer\">RingBuffer</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a>,&nbsp;</span>",synthetic:true,types:["ringbuf::ring_buffer::RingBuffer"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ target/doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["ringbuf"] = {"name":"","files":["consumer.rs","lib.rs","producer.rs","ring_buffer.rs"]};
+createSourceSidebar();
